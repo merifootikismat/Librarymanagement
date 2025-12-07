be3ded98f149a581a018b23f25aed0131e1349c6
Neeraj <neerajdel98@gmail.com>
2025-12-07 21:22:46 +0530

    fixed fbs

--- routes/book_route.py
+++ routes/book_route.py
@@ -1,27 +1,28 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from services.book_service import list_book, get_book, create_book, update_book, delete_book
+from sql.database import get_db
 
 book_router = APIRouter(prefix="/book", tags=["books"])
 
 @book_router.get("/", summary="List all books")
-def list_all():
-    return list_book()
+def list_all(db = Depends(get_db)):
+    return list_book(db)
 
 @book_router.get("/{book_id}",)
-def single_book(book_id: int):
-    book = get_book(book_id)
+def single_book(book_id: int,db = Depends(get_db)):
+    book = get_book(db, book_id)
     if book:
         return book
     else:
         return {"error": "Book not found"}
 
 @book_router.post("/", summary="Create a new book")
-def add_book(data: dict):
-    return create_book(data)
+def add_book(data: dict,db = Depends(get_db)):
+    return create_book(db,data)
 
 @book_router.put("/{book_id}", summary="Update a book")
-def modify_book(book_id: int, data: dict):
-    updated_book = update_book(book_id, data)
+def modify_book(book_id: int, data: dict,db = Depends(get_db)):
+    updated_book = update_book(db, book_id, data)
     if updated_book:
         return updated_book
     else:
@@ -29,8 +30,8 @@ def modify_book(book_id: int, data: dict):
 
 
 @book_router.delete("/{book_id}", summary="Delete a book")
-def remove_book(book_id: int):
-    deleted = delete_book(book_id)
+def remove_book(book_id: int,db = Depends(get_db)):
+    deleted = delete_book(db, book_id)
     if deleted:
         return {"message": "Book deleted successfully"}
     else:
